32d0cb6121d130631f33ba0ee1f93c260920b677
深蓝喜欢 <oncwnuL90BoEAiyyYXrdyRkdW_V8@git.weixin.qq.com>
2020-03-01 20:19:32 +0000

    照片墙总共25张照片

--- miniprogram/pages/photo/photo.js
+++ miniprogram/pages/photo/photo.js
@@ -3,82 +3,52 @@ Page({
     dataSet: [
       {
         id: '01',
-        // backgroundColor: '#ffffff',
-        likedCount: 19,
-        liked: true,
         images: ['https://phdluffy.com/photo/images/gallery/01.jpg']
       },
       {
         id: '02',
-        // backgroundColor: '#ffffff',
-        likedCount: 19,
-        liked: true,
         images: ['https://phdluffy.com/photo/images/gallery/02.jpg']
       },
       {
         id: '03',
-        // backgroundColor: '#ffffff',
-        likedCount: 19,
-        liked: true,
         images: ['https://phdluffy.com/photo/images/gallery/03.jpg']
       },
       {
         id: '04',
-        // backgroundColor: '#ffffff',
-        likedCount: 19,
-        liked: true,
         images: ['https://phdluffy.com/photo/images/gallery/04.jpg']
       },
       {
         id: '05',
-        // backgroundColor: '#ffffff',
-        likedCount: 19,
-        liked: true,
         images: ['https://phdluffy.com/photo/images/gallery/05.jpg']
       },
       {
         id: '06',
-        // backgroundColor: '#ffffff',
-        likedCount: 19,
-        liked: true,
         images: ['https://phdluffy.com/photo/images/gallery/06.jpg']
       },
       {
         id: '07',
-        // backgroundColor: '#ffffff',
-        likedCount: 19,
-        liked: true,
         images: ['https://phdluffy.com/photo/images/gallery/07.jpg']
       },
       {
         id: '08',
-        // backgroundColor: '#ffffff',
-        likedCount: 19,
-        liked: true,
         images: ['https://phdluffy.com/photo/images/gallery/08.jpg']
       },
       {
         id: '09',
-        // backgroundColor: '#ffffff',
-        likedCount: 19,
-        liked: true,
         images: ['https://phdluffy.com/photo/images/gallery/09.jpg']
       },
       {
         id: '10',
-        // backgroundColor: '#ffffff',
-        likedCount: 19,
-        liked: true,
         images: ['https://phdluffy.com/photo/images/gallery/10.jpg']
       },
     ],
     brick_option: {
-      //showFullContent: false,
-      // backgroundColor:"#16A085",
+         showFullContent: false,
+      backgroundColor: "#F1F1F1",
       // forceRepaint: true,
          defaultExpandStatus: true,
       // imageFillMode:'aspectFill'
-      // columns: 3
+         columns: 2
       // icon: {
       //   fill: 'https://images.ifanr.cn/wp-content/uploads/2018/08/640-90-1024x576.jpeg',
       //   default:'https://images.ifanr.cn/wp-content/uploads/2018/08/640-90-1024x576.jpeg',
@@ -118,143 +88,99 @@ Page({
       dataSet: [
         {
           id: '01',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/01.jpg']
         },
         {
           id: '02',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/02.jpg']
         },
         {
           id: '03',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/03.jpg']
         },
         {
           id: '04',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/04.jpg']
         },
         {
           id: '05',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/05.jpg']
         },
         {
           id: '06',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/06.jpg']
         },
         {
           id: '07',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/07.jpg']
         },
         {
           id: '08',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/08.jpg']
         },
         {
           id: '09',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/09.jpg']
         },
         {
           id: '10',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/10.jpg']
         },
         {
           id: '11',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/11.jpg']
         },
         {
           id: '12',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/12.jpg']
         },
         {
           id: '13',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/13.jpg']
         },
         {
           id: '14',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/14.jpg']
         },
         {
           id: '15',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/15.jpg']
         },
         {
           id: '16',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/16.jpg']
         },
         {
           id: '17',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/17.jpg']
         },
         {
           id: '18',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/18.jpg']
         },
         {
           id: '19',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/19.jpg']
         },
         {
           id: '20',
-          // backgroundColor: '#ffffff',
-          likedCount: 19,
-          liked: true,
           images: ['https://phdluffy.com/photo/images/gallery/20.jpg']
+        },
+        {
+          id: '21',
+          images: ['https://phdluffy.com/photo/images/gallery/21.jpg']
+        }, {
+          id: '22',
+          images: ['https://phdluffy.com/photo/images/gallery/22.jpg']
+        }, {
+          id: '23',
+          images: ['https://phdluffy.com/photo/images/gallery/23.jpg']
+        }, {
+          id: '24',
+          images: ['https://phdluffy.com/photo/images/gallery/24.jpg']
+        }, {
+          id: '25',
+          images: ['https://phdluffy.com/photo/images/gallery/25.jpg']
         },
       ]
     })
